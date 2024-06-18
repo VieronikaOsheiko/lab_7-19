0d0163af8b32a4f02926cad446d552739edd8910
VieronikaOsheiko <123954371+VieronikaOsheiko@users.noreply.github.com>
2024-06-19 01:19:46 +0300

    feat: init

---
+++ blog/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
